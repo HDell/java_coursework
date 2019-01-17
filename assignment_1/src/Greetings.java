78b86d620d18cc9639bf26327a151ca498935040
Handell Desulme <handelldesulme@8c-85-90-bc-eb-12.wireless.temple.edu>
2019-01-17 12:10:53 -0500

    This is a new commit for what I originally planned to be amended

---
+++ assignment_1/src/Greetings.java
@@ -0,0 +1,12 @@
+// Handell Desulme
+// 1/17/2019
+// Assignment 1. Greetings
+//
+// This program prints a message mentioning my lab instructor by name to the console.
+//
+
+public class Greetings {
+    public static void main(String[] args){
+        System.out.println("Good morning, Professor X.");
+    }
+}
