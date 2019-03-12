78506754dda24ece56c72fce182d94f277623af4
Handell Desulme <handelldesulme@8c-85-90-bc-eb-12.wireless.temple.edu>
2019-03-12 16:39:19 -0400

    Creates Southie files

---
+++ assigment_6/src/assignmentSix/Southie.java
@@ -0,0 +1,10 @@
+package assignmentSix;
+
+// Handell Desulme
+// 3/15/2019
+// Assignment 6. A Southie Styles
+//
+// This program reads from a text source, converts dialogue to a predetermined accent/style, and outputs it to an output file.
+
+public class Southie {
+}
---
+++ assigment_6/src/assignmentSix/SouthieTest.java
@@ -0,0 +1,25 @@
+package assignmentSix;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SouthieTest {
+
+    @Test
+    public void isPlaceholder1(){
+        //First test;
+    }
+
+    @Test
+    public void isPlaceholder2(){
+        //Second test;
+    }
+
+    @Test
+    public void isPlaceholder3(){
+        //Third test;
+    }
+
+
+}
